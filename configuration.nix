8630ed0a55ea2d4d34a2c023e1365e98e4cb5b90
rafaelrc7 <rafaelrc7@gmail.com>
2021-12-04 19:31:03 -0300

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,154 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    timeout = 3;
+
+    systemd-boot = {
+      enable = true;
+      consoleMode = "max";
+      editor = false;
+      memtest86.enable = true;
+    };
+  };
+
+  networking.hostName = "omega"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  nix = {
+    autoOptimiseStore = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = false;
+  };
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+
+  systemd.network = {
+    enable = true;
+
+    netdevs = {
+      br0 = {
+        enable = true;
+        netdevConfig = { Name = "br0"; Kind = "bridge"; };
+      };
+    };
+
+    networks = {
+      wired0 = {
+        enable = true;
+        matchConfig = { Name = "enp*"; };
+        bridge = [ "br0" ];
+      };
+
+      br0 = {
+        enable = true;
+        matchConfig = { Name = "br0"; };
+        DHCP = "no";
+        address = [ "192.168.0.105/24" ];
+        gateway = [ "192.168.0.1" ];
+        dns = [ "1.1.1.1" "1.0.0.1" ];
+      };
+    };
+  };
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "br-abnt2";
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  # Configure keymap in X11
+  # services.xserver.layout = "br";
+  # services.xserver.xkbVariant = "abnt2";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.rafael = {
+      isNormalUser = true;
+      createHome= true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    systemPackages = with pkgs; [
+      neovim
+      htop
+      wget
+      neofetch
+    ];
+    shells = with pkgs; [ bashInteractive zsh ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services = {
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
+
